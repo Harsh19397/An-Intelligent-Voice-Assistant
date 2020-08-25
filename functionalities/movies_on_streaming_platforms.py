7b63738ddd1d8b59810f4179cf3a10f287f77ab0
Harsh Parashar <harshparashar19@gmail.com>
2020-08-26 00:20:54 +0530

    Updating the movies streaming functionality

--- functionalities/movies_on_streaming_platforms.py
+++ functionalities/movies_on_streaming_platforms.py
@@ -2,6 +2,8 @@
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize
 import webbrowser
+from google_apis_speech_text_conversion import SpeechToText as stt
+from google_apis_speech_text_conversion import TextToSpeech as tts
 
 def remove_stop_words(query):
     stop_words = set(stopwords.words('english')) 
@@ -27,7 +29,9 @@ def play_on_Netflix(query, assistant_name):
     url = "https://www.netflix.com/search?q="
     for s in query:
         url = url + s + "+"
-    url = url[:-1]         
+    url = url[:-1]        
+    print("{}: Here you go.".format(assistant_name))
+    tts.speak("Here you go!")
     #Openning up in the chrome browser 
     webbrowser.register('chrome',
                         None,
@@ -45,6 +49,8 @@ def play_on_Prime(query, assistant_name):
         url = url + s + "+"
     url = url[:-1]
     url = url + "&ie=UTF8"         
+    print("{}: Here you go.".format(assistant_name))
+    tts.speak("Here you go!")
     #Openning up in the chrome browser 
     webbrowser.register('chrome',
                         None,
@@ -61,6 +67,8 @@ def play_on_Hotstar(query, assistant_name):
     for s in query:
         url = url + s + "%20"
     url = url[:-3]
+    print("{}: Here you go.".format(assistant_name))
+    tts.speak("Here you go!")
     #Openning up in the chrome browser 
     webbrowser.register('chrome',
                         None,
