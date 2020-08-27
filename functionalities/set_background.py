5456e30dabb1a77d205fd1f796642f9ce748421e
Harsh Parashar <harshparashar19@gmail.com>
2020-08-28 00:57:49 +0530

    Updated the set_background functionality

--- functionalities/set_background.py
+++ functionalities/set_background.py
@@ -4,8 +4,11 @@ from google_apis_speech_text_conversion import TextToSpeech as tts
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
+import glob
+import os
 import webbrowser, time
 import pyautogui
+import random
 
 def set_Background_Image(assistant_name):
     print("{}: What background image you want to set?".format(assistant_name))
@@ -18,6 +21,12 @@ def set_Background_Image(assistant_name):
     clean = re.sub(r'[^ a-z A-Z 0-9]', " ", text)
     clean = re.sub(r'search', "", clean)
     clean = re.sub(r'please', "", clean)
+    clean = re.sub(r'would', "", clean)
+    clean = re.sub(r'like', "", clean)
+    clean = re.sub(r'set', "", clean)
+    clean = re.sub(r'wallpaper', "", clean)
+    clean = re.sub(r'background', "", clean)
+    clean = re.sub(r'change', "", clean)
     word_tokens = word_tokenize(clean)
     
     #Removing the stopwords
@@ -39,16 +48,29 @@ def set_Background_Image(assistant_name):
     webbrowser.get('chrome').open(url)
     
     #Saving the first image
-    time.sleep(5)
-    pyautogui.click(x = 200, y=600, button='right')
     time.sleep(3)
-    save_image = pyautogui.locateOnScreen('./functionalities/images/save_image_as.png')
-    save_image_as_location = pyautogui.center(save_image)
-    pyautogui.click(save_image_as_location)
-    time.sleep(5)
-    pyautogui.write('background_set_{}'.format(text)) 
-    pyautogui.press('enter')
-    time.sleep(5)
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, r'C:\Users\Harsh Parashar\Downloads\background_set_{}.jfif'.format(text) , 0)
+    #Setting the orientation
+    landscape_options = pyautogui.locateOnScreen('./functionalities/images/landscape.png')
+    landscape_options_as_location = pyautogui.center(landscape_options)
+    pyautogui.click(landscape_options_as_location)
+    time.sleep(1)
+    x_land, y_land = pyautogui.position()
+    pyautogui.click(x_land, y_land+80)
+    #Scrolling a bit for random selection
+    random_scroll = random.randint(1, 10)
+    pyautogui.scroll(-500*random_scroll)
+    time.sleep(2)
+    random_x = random.randint(350, 700)
+    pyautogui.click(x = random_x, y=450)
+    time.sleep(2)
+    #Saving the image
+    download_image = pyautogui.locateOnScreen('./functionalities/images/download.png')
+    download_image_as_location = pyautogui.center(download_image)
+    pyautogui.click(download_image_as_location)
+    #Setting up the background
+    time.sleep(4)
+    list_of_files = glob.glob(r'C:\Users\Harsh Parashar\Downloads\*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, latest_file , 0)
     print("{}: Background has been set.".format(assistant_name))
     tts.speak("Background has been set.")
